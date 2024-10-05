6d96960f9be2f21f154f031a9126a401157b3a33
YoumnaRefaat <youmnarefaat884@gmail.com>
2024-10-05 20:23:03 +0300

    modified the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_weather_cubit.dart';
+import 'package:weather_app/cubits/get_weather_states.dart';
+import 'package:weather_app/views/home_view.dart';
 
 void main() {
   runApp(const WeatherApp());
@@ -9,7 +13,26 @@ class WeatherApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return BlocProvider(
+      create: (context) => GetWeatherCubit(),
+      child: Builder(
+        builder: (context) => BlocBuilder<GetWeatherCubit, WeatherState>(
+          builder: (context, state) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                primarySwatch: getThemeColor(
+                  BlocProvider.of<GetWeatherCubit>(context)
+                      .weatherModel
+                      ?.weatherCondition,
+                ),
+              ),
+              home: const HomePage(),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
 
